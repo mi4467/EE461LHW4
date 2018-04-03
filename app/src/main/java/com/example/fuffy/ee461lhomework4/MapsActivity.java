82faa1c9db12fc19c69feb71b6aaf6a68f05e80a
Mustafa <irfan24@utexas.edu>
2018-04-02 23:32:45 -0500

    second commit

--- app/src/main/java/com/example/fuffy/ee461lhomework4/MapsActivity.java
+++ app/src/main/java/com/example/fuffy/ee461lhomework4/MapsActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -48,8 +49,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         Button enter = findViewById(R.id.enter_location_button_maps);
         enter.setOnClickListener(new View.OnClickListener() {
@@ -62,18 +63,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    public void resetText(Address a){
+        StringBuilder newText = new StringBuilder();
+        newText.append("Location Info \n\n");
+        newText.append("Country: ");
+        newText.append(a.getCountryName() + "\n");
+
+        newText.append("Address: ");
+        for(int i = 0; i<a.getMaxAddressLineIndex(); i++) {
+            newText.append(a.getAddressLine(i) + "\n");
+        }
+
+        TextView textView = findViewById(R.id.first_extra_credit);
+        textView.setText(newText);
+    }
+
 
     public void search()  {
         EditText location = findViewById(R.id.location_text_box_maps);
         String loc = location.getText().toString();
         try {
             if (loc != null || !loc.equals("")) {
+                mMap.clear();
                 Geocoder geocoder = new Geocoder(this);
                 List<Address> addressList = geocoder.getFromLocationName(loc, 1);
                 Address address = addressList.get(0);
                 LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
                 mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                resetText(address);
             }
         }
         catch(Exception e){
